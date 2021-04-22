6768c5d198aa240e99f43270b84b38bdf2618bbc
ChrisMayes22 <cpmayes@gmail.com>
2021-04-21 20:17:11 -0400

    First draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
+
 import './App.css';
+import updateInsanities from './utils/ariana' 
+import React, { useState } from 'react'
+
 
 function App() {
+
+  const [days, setDays] = useState(0)
+  const [ariana, setAriana] = useState(1);
+  const [quelani, setQuelani] = useState(1)
+  const [arianaActivity, setArianaActivity] = useState("none");
+  const [quelaniKills, setQuelaniKills] = useState("0");
+
+  function submitHandler(e){
+      e.preventDefault();
+      const sing = arianaActivity === "sing" ? true : false;
+      const draw = arianaActivity === "draw" ? true : false;
+      const [tmpAriana, tmpQuelani] = updateInsanities(ariana, quelani, sing, draw, quelaniKills);
+      setAriana(tmpAriana);
+      setQuelani(tmpQuelani);
+      setDays(days + 1);
+  }
+
+  function dayHandler(e) {
+    console.log(e.target.value);
+    const days = []
+    for(let i = 0; i < parseInt(e.target.value); i++){
+      days.push(0);
+    }
+    setDays(days);
+    console.log(days);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div>
+          ARIANA: {ariana}
+        </div>
+        <div>
+          QUELANI: {quelani}
+        </div>
+        <div>
+          DAYS CALCULATED: {days}
+        </div>
+        <div>How many Days to Calculate?</div>
+        <input type="number" defaultValue="1" onChange={(e) => dayHandler(e)}/>
+        <form name="insanityForm" onSubmit={(e) => submitHandler(e)}>
+          <div>
+              <label>What does Ariana do on day {days}?</label>
+              <div className={"input_container"}>
+                  <input onClick = {() => setArianaActivity("sing")}
+                      type="radio"
+                      name={`ariana`}
+                      key={`r_sing-`}
+                      id={`r_sing-`}
+                      value="sing"/>
+                  <label htmlFor={`r_sing-`} children="sing"/>
+              </div>
+              <div className={"input_container"}>
+                  <input onClick = {() => setArianaActivity("draw")}
+                      type="radio"
+                      name={`ariana`}
+                      key={`r_draw-`}
+                      id={`r_draw-`}
+                      value="draw"/>
+                  <label htmlFor={`r_draw-`} children="draw"/>
+              </div>
+              <div className={"input_container"}>
+                  <input onClick = {() => setArianaActivity("none")}
+                      type="radio"
+                      name={`ariana`}
+                      key={`r_none-`}
+                      id={`r_none-`}
+                      value="none"/>
+                  <label htmlFor={`r_none-`} children="none"/>
+              </div>
+
+              <label>How many killing blows does Quelani land on day {days}?</label>
+              <div className={"input_container"}>
+                  <input onClick = {() => setQuelaniKills("0")} 
+                      type="radio"
+                      name={`quelani`}
+                      key={`r_no-kills-`}
+                      id={`r_no-kills-`}
+                      value="0-1"/>
+                  <label htmlFor={`r_no-kills-`} children="0-1"/>
+              </div>
+              <div className={"input_container"}>
+                  <input onClick = {() => setQuelaniKills("2")}
+                      type="radio"
+                      name={`quelani`}
+                      key={`r_two-kills-`}
+                      id={`r_two-kills-`}
+                      value="2"/>
+                  <label htmlFor={`r_two-kills-`} children="2"/>
+              </div>
+              <div className={"input_container"}>
+                  <input onClick = {() => setQuelaniKills("3")}
+                      type="radio"
+                      name={`quelani`}
+                      key={`r_three-kills-`}
+                      id={`r_three-kills-`}
+                      value="3+"/>
+                  <label htmlFor={`r_three-kills-`} children="3+"/>
+              </div>
+          </div>
+          <input type="submit"/>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/utils/ariana.js
@@ -0,0 +1,49 @@
+
+function getRandom(min, max) {
+    const num = Math.floor(Math.random() * (max+1-min) + min)
+    return num;
+}
+
+
+function getArianaInsanity(ariana, sing=false, draw=false){
+    if(draw){
+        const dice = getRandom(1, 10);
+        return dice < ariana ? 1 : ariana + 1
+    } else if(sing){
+        return ariana + 3
+    } else {
+        return ariana + 1
+    }
+}
+
+function getQuelaniInsanity(quelani, kills, sing){
+
+    switch(kills){
+        case "0":
+            console.log("QUELANI", quelani)
+            quelani += 1;
+            break;
+        case "1": 
+            quelani += 1;
+            break;
+        case "2":
+            quelani += getRandom(1,2);
+            break;
+        default:
+            quelani += getRandom(1, 3);
+    }
+
+    return quelani - (sing ? 4 : 0) 
+}
+
+function updateInsanities(ariana, quelani, sing, draw, kills){
+    console.log(`ARIANA: ${ariana}, SING: ${sing}, DRAW: ${draw}, KILLS: ${kills}`)
+    ariana = getArianaInsanity(ariana, sing, draw);
+    quelani = getQuelaniInsanity(quelani, kills, sing);
+    quelani = quelani <= 0 ? 1 : quelani;
+    ariana = ariana <= 0 ? 1 : ariana;
+
+    return [ariana, quelani]
+}
+
+export default updateInsanities;
